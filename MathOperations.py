a0dd6d5505c0aeea0551e2a1d2c6a37240c9288b
CopperUke <saami.siddiquioutlook.com@gmail.com>
2025-02-25 13:35:31 -0500

    Create MathOperations.py

---
+++ MathOperations.py
@@ -0,0 +1,31 @@
+#Saami's Work
+#Date Modified: Febuary 21, 2025
+#Description: Mathematical Operators
+
+import math
+#Addition
+sum_add = 2 + 23
+print(sum_add)
+
+#Subtraction
+difference = 2 - 23
+print(difference)
+
+#Multiplication
+product = 2 * 3 * 4
+print(product)
+
+#Division
+quotient = 24 / 8
+print(quotient)
+#For Integer (Division):
+print(int(quotient))
+
+#Square Root
+
+square = math.sqrt(25)
+print(square)
+
+#Exponents
+exponent = (2**4)
+print(exponent)
